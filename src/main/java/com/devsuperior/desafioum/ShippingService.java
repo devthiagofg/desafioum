89e7bdf82005a66b51935cffcdbd75a8934427e5
devthiagofg <dev.thiagofg@gmail.com>
2025-09-13 23:27:42 -0300

    ShippingService

---
+++ src/main/java/com/devsuperior/desafioum/ShippingService.java
@@ -0,0 +1,20 @@
+package com.devsuperior.desafioum;
+
+import com.devsuperior.desafioum.entites.Order;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShippingService {
+
+    public double shipment(Order order) {
+
+        if (order.getBasic() < 100.00) {
+            return 20.0;
+        } else if (order.getBasic() < 200.0) {
+            return 12.0;
+        }
+        else {
+            return 0.0;
+        }
+    }
+}
