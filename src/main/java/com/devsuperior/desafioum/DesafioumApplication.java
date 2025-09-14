9d6a95cefb470d6c11fa50336cc667eaf28b1c5d
devthiagofg <dev.thiagofg@gmail.com>
2025-09-13 23:48:27 -0300

    implementation

--- src/main/java/com/devsuperior/desafioum/DesafioumApplication.java
+++ src/main/java/com/devsuperior/desafioum/DesafioumApplication.java
@@ -1,13 +1,43 @@
 package com.devsuperior.desafioum;
 
+import com.devsuperior.desafioum.entites.Order;
+import com.devsuperior.desafioum.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioumApplication {
+public class DesafioumApplication implements CommandLineRunner {
+
+    @Autowired
+    private OrderService orderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioumApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Order order1 = new Order(1034, 150.00, 20.0);
+        double total1 = orderService.total(order1);
+        System.out.println("Pedido código " + order1.getCode());
+        System.out.printf("Valor total: R$ %.2f%n", total1);
+
+        System.out.println();
+
+        Order order2 = new Order(2282, 800.00, 10.0);
+        double total2 = orderService.total(order2);
+        System.out.println("Pedido código " + order2.getCode());
+        System.out.printf("Valor total: R$ %.2f%n", total2);
+
+        System.out.println();
+
+        Order order3 = new Order(1309, 95.90, 0.0);
+        double total3 = orderService.total(order3);
+        System.out.println("Pedido código " + order3.getCode());
+        System.out.printf("Valor total: R$ %.2f%n", total3);
+
+    }
 }
